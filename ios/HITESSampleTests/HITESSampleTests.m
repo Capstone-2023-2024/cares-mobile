dbd3fd5dee46aa0c19b078c2a27f1ac8e5ab56b8
Resyntech <carranzagcarlo@gmail.com>
2023-06-30 15:05:54 +0800

    renaming application name from HITESSample -> cics

--- ios/HITESSampleTests/HITESSampleTests.m
+++ ios/HITESSampleTests/HITESSampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HITESSampleTests : XCTestCase
+@interface cicsTests : XCTestCase
 
 @end
 
-@implementation HITESSampleTests
+@implementation cicsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
