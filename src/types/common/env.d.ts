f1e89a589cb17925f8a8c2726ee851bc0e491b10
Resyntech <carranzagcarlo@gmail.com>
2023-12-07 01:55:13 +0800

    Build failure....

--- src/types/common/env.d.ts
+++ src/types/common/env.d.ts
@@ -2,4 +2,6 @@ declare module '@env' {
   export const NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET: string;
   export const FIRESTORE_WEB_CLIENT_ID: string;
   export const NEXT_PUBLIC_ONESIGNAL_APP_ID: string;
+  export const NEXT_PUBLIC_ONESIGNAL_REST_API_KEY: string;
+  export const NEXT_PUBLIC_ONESIGNAL_DEFAULT_ANDROID_CHANNEL_ID: string;
 }
--- src/utils/media.ts
+++ src/utils/media.ts
@@ -1,28 +1,39 @@
-import RNFetchBlob from 'rn-fetch-blob';
+import BlobCourier from 'react-native-blob-courier';
 
 async function downloadPhoto(FILE_URL: string) {
   let date = new Date();
-  let file_ext = '.jpg';
+  const request = {
+    android: {
+      useDownloadManager: true, // <--- set useDownloadManager to "true"
+    },
+    filename: `${date.getTime()}.jpg`,
+    method: 'GET',
+    mimeType: 'image/jpeg',
+    url: FILE_URL,
+  };
+  const fetchedResult = await BlobCourier.fetchBlob(request);
+  console.log({fetchedResult});
+  // let file_ext = '.jpg';
 
   // config: To get response by passing the downloading related options
   // fs: Root directory path to download
-  const {config, fs} = RNFetchBlob;
-  let RootDir = fs.dirs.PictureDir;
-  let options = {
-    fileCache: true,
-    addAndroidDownloads: {
-      path:
-        RootDir +
-        '/file_' +
-        Math.floor(date.getTime() + date.getSeconds() / 2) +
-        file_ext,
-      description: 'downloading file...',
-      notification: true,
-      // useDownloadManager works with Android only
-      useDownloadManager: true,
-    },
-  };
-  return await config(options).fetch('GET', FILE_URL);
+  // const {config, fs} = RNFetchBlob;
+  // let RootDir = fs.dirs.PictureDir;
+  // let options = {
+  //   fileCache: true,
+  //   addAndroidDownloads: {
+  //     path:
+  //       RootDir +
+  //       '/file_' +
+  //       Math.floor(date.getTime() + date.getSeconds() / 2) +
+  //       file_ext,
+  //     description: 'downloading file...',
+  //     notification: true,
+  //     // useDownloadManager works with Android only
+  //     useDownloadManager: true,
+  //   },
+  // };
+  // return await config(options).fetch('GET', FILE_URL);
 }
 
 export {downloadPhoto};
