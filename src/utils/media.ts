ede318f4b9ef75cee3a0bc2942f3cda4a87c3c13
Resyntech <carranzagcarlo@gmail.com>
2023-12-07 07:21:35 +0800

    Remove rn-fetch-blob due to package issues, detaching from workspace

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -1,28 +1,27 @@
-import RNFetchBlob from 'rn-fetch-blob';
-
 async function downloadPhoto(FILE_URL: string) {
-  let date = new Date();
-  let file_ext = '.jpg';
+  console.log(FILE_URL);
+  // let date = new Date();
+  // let file_ext = '.jpg';
 
-  // config: To get response by passing the downloading related options
-  // fs: Root directory path to download
-  const {config, fs} = RNFetchBlob;
-  let RootDir = fs.dirs.PictureDir;
-  let options = {
-    fileCache: true,
-    addAndroidDownloads: {
-      path:
-        RootDir +
-        '/file_' +
-        Math.floor(date.getTime() + date.getSeconds() / 2) +
-        file_ext,
-      description: 'downloading file...',
-      notification: true,
-      // useDownloadManager works with Android only
-      useDownloadManager: true,
-    },
-  };
-  return await config(options).fetch('GET', FILE_URL);
+  // // config: To get response by passing the downloading related options
+  // // fs: Root directory path to download
+  // const {config, fs} = RNFetchBlob;
+  // let RootDir = fs.dirs.PictureDir;
+  // let options = {
+  //   fileCache: true,
+  //   addAndroidDownloads: {
+  //     path:
+  //       RootDir +
+  //       '/file_' +
+  //       Math.floor(date.getTime() + date.getSeconds() / 2) +
+  //       file_ext,
+  //     description: 'downloading file...',
+  //     notification: true,
+  //     // useDownloadManager works with Android only
+  //     useDownloadManager: true,
+  //   },
+  // };
+  // return await config(options).fetch('GET', FILE_URL);
 }
 
 export {downloadPhoto};
